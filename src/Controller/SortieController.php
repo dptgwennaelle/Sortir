361e1dd3c6312f993f6a63739f56c2c492855334
Gwen <depoorter.gwenaelle@gmail.com>
2024-02-06 06:00:19 +0100

    Modification code

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-
+    #[Route('/', name: 'sortir_home')]
+ public function home(){
+        return $this->render('sortie/home.html.twig', [
+        ]);
+ }
 }
