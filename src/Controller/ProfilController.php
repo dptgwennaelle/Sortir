7d69efb92825c48a859ef4eec7cc166b63fac7ed
dptgwennaelle <depoorter.gwenaelle@gmail.com>
2024-02-09 03:51:46 +0100

    Ajout fonctionnalités

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,9 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Entity\Wish;
+use App\Form\ProfilType;
+use App\Form\WishFormType;
+use App\Repository\ParticipantRepository;
+use App\Repository\WishRepository;
+use App\Service\Censurator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
+
+    #[Route('/profil', name: 'profil_liste')]
+    public function liste(ParticipantRepository $participantRepository): Response
+    {
+        $participantId = $this->getUser()->getUserIdentifier();
+        $profil = $participantRepository->findOneByIdentifiant($participantId);
+        return $this->render('profil/liste.html.twig', [
+            'profil'=>$profil
+        ]);
+    }
+    #[Route('/profil/detail/{id}', name: 'profil_detail')]
+    public function details(int $id, ParticipantRepository $participantRepository): Response
+    {
+        $profil = $participantRepository->find($id);
+        if (!$profil){
+            throw $this->createNotFoundException('Oh no!!!');
+        }
+        return $this->render('profil/detail.html.twig', [
+            'profil'=>$profil
+        ]);
+    }
+    #[Route('/profil/create', name: 'profil_create')]
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): Response
+    {
+        $user = $this->getUser()->getUserIdentifier();
+        $profil = new Participant();
+        $profilForm = $this->createForm(ProfilType::class, $profil);
+
+        $profilForm->handleRequest($request);
+
+        if ($profilForm->isSubmitted() && $profilForm->isValid()) {
+            $entityManager->persist($profil);
+            $entityManager->flush();
+
+            //$this->addFlash('Succès', 'Voeux ajouté, bien joué!');
+            return $this->redirectToRoute('profil/create.html.twig', ['id' => $profil->getId()]);
+        }
+        return $this->render('wish/create.html.twig', [
+            'profilForm'=>$profilForm->createView(),
+        ]);
+    }
 
 }
--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,14 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-    #[Route('/Sortie', name: 'sortir_home')]
+    #[Route('/Sortie/', name: 'sortir_home')]
  public function home(){
         return $this->render('sortie/home.html.twig', [
         ]);
  }
+    #[Route('/Sortie/liste', name: 'sortir_liste')]
+    public function liste(SortieRepository $sortieRepository){
+        $sortieList = $sortieRepository->findAll();
+        return $this->render('sortie/liste.html.twig', [
+            'sorties'=>$sortieList
+        ]);
+    }
+
+    #[Route('/Sortie/create', name: 'sortir_create')]
+    public function create(){
+        return $this->render('sortie/create.html.twig', [
+        ]);
+    }
+
 }
