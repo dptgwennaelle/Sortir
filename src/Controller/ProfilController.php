09b2d1c3798711c5619e4d62bc0e167b51cea07c
chevalierMSIO <112487236+chevalierMSIO@users.noreply.github.com>
2024-02-13 09:56:12 +0100

    controller

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,9 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
+use App\Repository\SerieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
+/*Petit pense bête à usage personnel:
+ *
+ * Un controller pour chaque type de page.
+Une action affiche une vue.
 
+Dans chaque controller:
+    - 1 route = 1 action -> méthode */
+
+
+            // Je suis dans le profil controller
+
+    // 1ère action, je souhaite afficher le profil
+
+            //A. La route
+    #[Route('/Participant/details/{id}', name: 'participant_details')]
+    public function details(int $id, ParticipantRepository $participantRepository): Response
+    {
+        $participant = $participantRepository->find($id);
+
+        if(!$participant){
+            throw $this->createNotFoundException('Oh no!!!');
+        }
+        return $this->render('participant/details.html.twig', [
+            "profil"=> $participant
+        ]);
+    }
 }
--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
     #[Route('/', name: 'sortir_home')]
- public function home(){
+    public function home(){
         return $this->render('sortie/home.html.twig', [
         ]);
- }
+    }
 }
